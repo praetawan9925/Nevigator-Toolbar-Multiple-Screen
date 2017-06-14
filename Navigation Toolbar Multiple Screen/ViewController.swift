8dfda2fb24da289b94aee326e41c9105acc3c531
student08 <praetawan9925@snru.ac.th>
2017-06-14 16:52:41 +0700

    Project Sussess1

--- Navigation Toolbar Multiple Screen/ViewController.swift
+++ Navigation Toolbar Multiple Screen/ViewController.swift
@@ -9,11 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Implicit
+    var intNumber = 1
+    
+    
+    
+    @IBAction func IncreaseNumber(_ sender: Any){
+        showMessage(strMessage: "Click increaseNumber")
+        intNumber += 1
+        intNumber = checkNumber(intNumber: intNumber)
+        showNumber(intNumber: intNumber)
+    }
+    
+    @IBAction func decreaseNumber(_ sender: Any) {
+        showMessage(strMessage: "Click decreaseNumber")
+        intNumber -= 1
+       intNumber = checkNumber(intNumber: intNumber)
+        showNumber(intNumber: intNumber)
+    }
+    
+    
+    @IBAction func resetNumber(_ sender: Any) {
+        showMessage(strMessage: "Click resetNumber")
+        intNumber = 1
+        showNumber(intNumber: intNumber)
+    }
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }  //Main Method
+    
+    func checkNumber(intNumber: Int) -> Int {
+        var intResult = intNumber
+        
+        //At 11
+        if intNumber == 11 {
+        intResult = -10
+        
+        }
+        //At -11
+        if intNumber == -11 {
+        intResult = 10
+        }
+     return intResult
     }
+ 
+
+    func showNumber(intNumber: Int) -> Void {
+        let strNumber = String(intNumber)
+        numberLabel.text = strNumber
+    }
+    
+    func showMessage(strMessage: String) -> Void {
+        print("Message ==> \(strMessage)")
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -24,5 +81,6 @@ class ViewController: UIViewController {
     
     
 
-}
 
+
+}
